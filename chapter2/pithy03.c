04e963d95947aa26d5f4ef125380f9139063b32a
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-01-17 19:15:58 -0800

    Chapter 2 & 3 added

---
+++ chapter2/pithy03.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BSIZE 256
+
+int main()
+{
+	const char filename[] = "pithy.txt";
+	FILE *fp;
+	char buffer[BSIZE];
+	char *r;
+	char *entry;
+	int items, x;
+	char **list_base;
+
+	fp = fopen(filename,"r");
+	if( fp==NULL )
+	{
+		fprintf(stderr, "Unable to open file %s\n",filename);
+		exit(1);
+	}
+
+	list_base = (char **)malloc(sizeof(char *) * 100);
+	if ( list_base==NULL )
+	{
+		fprintf(stderr, "Unable to allocate memory\n");
+		exit(1);
+	}
+	items = 0;
+	while( !feof(fp) )
+	{
+		r = fgets(buffer,BSIZE,fp);
+		entry = (char *)malloc(sizeof(char) * strlen(buffer)+1);
+		if( entry==NULL )
+		{
+			fprintf(stderr, "Unable to allocate  memory\n");
+		}
+		strcpy(entry, buffer);
+		*(list_base+items) = entry;
+		items++;
+		if(  r==NULL )
+			break;
+		printf("%s",entry);
+	}
+	fclose(fp);
+
+	for( x=0; x<items; x++ )
+		printf("%s",*(list_base+x));
+
+	return(0);
+}
